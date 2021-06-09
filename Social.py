75713e92909eb786f81a64b21219be3814e0a614
Sam <scarlso5@uncc.edu>
2021-06-08 21:17:21 -0400

    Columns for Data settings, more deduction explanation

--- Social.py
+++ Social.py
@@ -164,8 +164,8 @@ bigGraph.write(graph)
 # Paycheck dataframe #
 ######################
 paycheckAppdata, paycheckAppDesc = paycheckApp.beta_columns(2)
-paycheckAppDesc.header('Paycheck approximations')
-paycheckAppDesc.text("'Payout' represents the approximate Take-home income after all deductions and taxes")
+paycheckApp.header('Paycheck approximations')
+paycheckAppDesc.subheader("'Payout' represents the approximate Take-home income after all deductions and taxes")
 paycheckAppDesc.subheader('How is the payout calculated?')
 paycheckAppDesc.text("Income consists of Credit Tips, Cash tips, and an Hourly Wage.\n5% of Alcohol Sales go to Bartenders, deducted from Credit Tips.\n3% of Food Sales go to Dining Room Attendants, deducted from Credit Tips.\nFederal Tax, State Tax, Medicare, Social Security are all deducted.")
 
